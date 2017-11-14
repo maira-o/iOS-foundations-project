78bcf51847921f858d5bf2fc149665c24af473d1
mairadoliveira <mairasetecores@gmail.com>
2017-11-14 10:15:03 -0200

    creating filter screen

--- nossoApp/FilterViewController.swift
+++ nossoApp/FilterViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FilterViewController: UIViewController {
+class FilterViewController: UIViewController, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
     var dataArray = [DataModelFilter]()
@@ -16,8 +16,23 @@ class FilterViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.tableView.dataSource = self
+        
+        dataArray.append(DataModelFilter(restrictionName: "Gluten Free"))
+        dataArray.append(DataModelFilter(restrictionName: "Lactose Free"))
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+            let cell = tableView.dequeueReusableCell(withIdentifier: "myRestrictionCell", for: indexPath) as! DataCellFilter
+            cell.data = dataArray[indexPath.row]
+            return cell
+    }
+        
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
